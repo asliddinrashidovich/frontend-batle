347f6f4b1266b124946e598d6f1a9093cc8ae47c
Asliddin <asliddinrashidovich7@gmail.com>
2025-06-01 13:58:04 +0500

    the pages is add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import MainLayout from "./layout/main-layout"
+import LoginPage from "./pages/login-page"
+import RegisterPage from "./pages/register-page"
+import DashboardPage from "./pages/dashboard"
+
 function App() {
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout/>}>
+        <Route index element={<LoginPage/>}/>
+        <Route path="register" element={<RegisterPage/>}/>
+        <Route path="dashboard" element={<DashboardPage/>}/>
+      </Route>
+    )
+  )
   return (
-    <div>App</div>
+    <>
+      <RouterProvider router={routes}/>
+    </>
   )
 }
 
